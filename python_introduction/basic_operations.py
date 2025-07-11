5ab9c9958e53ec12304726adb747eb1639e7b22f
Emy-gold <imanelgarate@gmail.com>
2025-07-11 20:47:19 +0100

    first commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+diffence = number1 - number2
+product = number1 * number2
+print("Addition of " +str(number1) +" and " +str(number2) + " is " + str(sum))
+print("Subtraction of " + str(number1) + " and " + str(number2) + " is " + str(diffence))
+print("Multiplication of " + str(number1) + " and " + str(number2) + " is " + str(product))
