7aec445b43f7b168291d4d00ea992f2a39c413d9
Emy-gold <imanelgarate@gmail.com>
2025-07-20 21:45:26 +0100

    add

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,15 @@
+num1 = input("Enter the first number:")
+num2 = input("Enter the second number:")
+operation = input("Enter the operation (+, -, *, /): ")
+if operation == "+":
+    result = float(num1) + float(num2)
+elif operation == "-":
+    result = float(num1) - float(num2)
+elif operation == "*":
+    result = float(num1) * float(num2)
+elif operation == "/":
+    result = float(num1) / float(num2)
+    if float(num2) == 0:
+        result = "Cannot divide by zero"
+else:
+    result = "Invalid operation"
