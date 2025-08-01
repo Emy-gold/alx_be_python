33a2580c9b70ffdf4b6debbad4b7409f63dea6c8
Emy-gold <imanelgarate@gmail.com>
2025-08-01 13:07:36 +0100

    bfke

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,8 +6,8 @@ def perform_operation(num1, num2, operation):
     elif operation == 'multiply':
         return num1 * num2
     elif operation == 'divide':
-        return num1 / num2
         if num2 == 0 :
             print("We cant devide on 0 please choose another number")
+        return num1 / num2
     else:
         print("Please choose one of the mentioned operations!")
